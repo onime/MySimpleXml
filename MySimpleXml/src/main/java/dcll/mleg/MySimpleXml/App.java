56ec0a3606ed3341a6b4003adfaef5432a786c67
etu5 <etu5@ensdl2012-12.edu.ups-tlse.fr>
2013-02-22 11:44:47 +0100

    #1 jay a été renvoyé

--- MySimpleXml/src/main/java/dcll/mleg/MySimpleXml/App.java
+++ MySimpleXml/src/main/java/dcll/mleg/MySimpleXml/App.java
@@ -1,13 +1,28 @@
 package dcll.mleg.MySimpleXml;
 
+import java.io.IOException;
+
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.ParsingException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+    	try {
+    		  Builder parser = new Builder();
+    		  Document doc = parser.build("http://www.cafeconleche.org/");
+    		}
+    		catch (ParsingException ex) {
+    		  System.err.println("Cafe con Leche is malformed today. How embarrassing!");
+    		}
+    		catch (IOException ex) {
+    		  System.err.println("Could not connect to Cafe con Leche. The site may be down.");
+    		}
+        System.out.println("Hello World!");
     }
 }
